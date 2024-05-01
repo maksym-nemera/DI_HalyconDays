1dd8bab991bca5bc60910db488fda0693c617067
Maksym Nemera <nickmitchell@ukr.net>
2024-05-01 19:30:53 +0300

    Updates

---
+++ index.3cbcf5f8.js
@@ -0,0 +1,5 @@
+$("#disign-slide").owlCarousel({loop:!0,margin:10,nav:!0,responsive:{0:{items:1},600:{items:1},1e3:{items:1}}}),// slider2
+$("#slider-two").owlCarousel({loop:!0,margin:10,nav:!0,responsive:{0:{items:1},600:{items:2},1e3:{items:3}}}),// slider3
+$("#slider-three").owlCarousel({loop:!0,margin:10,nav:!0,responsive:{0:{items:1},600:{items:2},1e3:{items:3}}});//# sourceMappingURL=index.3cbcf5f8.js.map
+
+//# sourceMappingURL=index.3cbcf5f8.js.map
