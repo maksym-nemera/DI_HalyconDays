b942bd1ded102ce5292e725d9a11470becaa6e3e
Maksym Nemera <nickmitchell@ukr.net>
2024-05-01 19:20:19 +0300

    add init code remove unrequire code

---
+++ src/js/script.js
@@ -0,0 +1,50 @@
+$('#disign-slide').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+// slider2
+$('#slider-two').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+// slider3
+$('#slider-three').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
